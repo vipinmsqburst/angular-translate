2f12d26c20c5561ec4b9342b250a53ac36b05003
Vipin M S <vipinm@qburst.com>
2019-12-17 11:50:23 +0530

    Implements translation and a login form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'multi-language';
+export class AppComponent implements OnInit {
+  private availableLanguages = ['en', 'ar'];
+  public selectedLanguage;
+  constructor(
+    public translate: TranslateService,
+    private meta: Meta) {
+    this.translate.addLangs([...this.availableLanguages]);
+    this.translate.setDefaultLang('en');
+    this.selectedLanguage = localStorage.getItem('lang') ? localStorage.getItem('lang'): this.translate.getBrowserLang().match(/ar|en/) ? this.translate.getBrowserLang() :'en';
+  }
+
+  ngOnInit() {
+    this.changeLanguage(this.selectedLanguage);
+  }
+
+  changeLanguage(lang) {
+    this.translate.use(lang);
+    this.meta.updateTag({name: 'lang', content: lang});
+    localStorage.setItem('lang', lang);
+  }
+
+  OnChangeLanguage() {
+    this.changeLanguage(this.selectedLanguage);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {TranslateHttpLoader} from '@ngx-translate/http-loader';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
+
+import { FormsModule } from '@angular/forms';
+
+
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -10,7 +22,17 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    }),
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
